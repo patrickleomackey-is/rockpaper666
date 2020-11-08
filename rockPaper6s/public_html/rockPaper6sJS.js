bace66e4469ce29b4097e2fec59350cd4af6a348
patrickleomackey-is <plmackey@my.niacc.edu>
2020-11-08 00:54:29 -0600

    rock paper 666's, just practice using a function to list the actual outcome of the programs throw, the radio buttons are giving me hell I will overcome, and make these buttons record the intended data upon form submission.

--- rockPaper6s/public_html/rockPaper6sJS.js
+++ rockPaper6s/public_html/rockPaper6sJS.js
@@ -2,11 +2,21 @@
  */
 let tie = [], cpWins=[], Uwins=[];radioButtons = [];
 
-let rock = 0;
-let paper = 1;
-let scissors =2;
-let Random = Number(Math.floor(Math.random()*3));
-document.write('<br>'+Random);
+
+function cpThrow(){ 
+    Random = Number(Math.floor(Math.random()*3));
+    
+    if (Random === 0){
+        document.write("It threw a rock!");
+    } else if (Random === 1){
+        document.write("It threw a paper ");}
+    else if(Random === 2){
+        document.write("It threw a scissors");}
+    }
+    
+    
+
+document.addEventListener("submit",cpThrow());
 
 //let userThrow = document.getElementsByTagName('input').checked;
  // document.write("   " + userThrow);
@@ -35,13 +45,13 @@ function Validate(){
     
     var radioButtons = document.getElementsByName("userThrow").checked;
     var i = 0;
-    while (i < radioButtons.length){
-        if(radioButtons[i].checked === true){
+    for (i=0;i < radioButtons.length;i++){
+        if (radioButtons[i].checked === true){
            Athrow = Number(radioButtons[i].value);
        }
-    ++i;
+  
    
-    if ((Athrow === 0)&&(Random === 0))
+    if ((Athrow === 0)&&(Random === '0'))
     {alert ("you tied");
         tie++;} 
      if ((Athrow === 0)&&(Random === '2'))
@@ -62,5 +72,5 @@ function Validate(){
     {cpWins++;}
                }        
            }        
-
+document.write(''+cpWins+" "+ Uwins + " "+ tie);
 
